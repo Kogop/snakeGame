0ba32c85163cfa8ead50f2d664d39bf0fbb667aa
nvo <nvo@pnzgru.ru>
2024-10-29 15:01:36 +0300

    comments and clarifications

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import pygame
 import sys
 import random
 
-
 # Game constants
 WIDTH, HEIGHT = 800, 600
 BLOCK_SIZE = 20
 SPEED = 10
 FOOD_SPAWN_RATE = 100
 
-
 # Game variables
-snake = [(200, 200), (220, 200), (240, 200)]  # initial snake position
+snake = [(200, 200), (220, 200), (240, 200)]  # initial snake position # snake's head is last one, snake starts from end of tuple
 direction = 'RIGHT'  # initial direction
 food = (400, 300)  # initial food position
 score = 0
+
 
 def draw_game():
     """Draws the game state on the screen"""
@@ -27,28 +26,31 @@ def draw_game():
     screen.blit(text, (10, 10))
     pygame.display.flip()
 
+
 def move_snake():
     """Moves the snake based on the current direction"""
 
-    head = snake[-1]
+    head = snake[-1]  #take last element(it's head)
     if direction == 'UP':
-        new_head = (head[0], head[1] - BLOCK_SIZE)
+        new_head = (head[0], head[1] - BLOCK_SIZE) # (x,y-1) less on top so less y = going top
     elif direction == 'DOWN':
         new_head = (head[0], head[1] + BLOCK_SIZE)
     elif direction == 'LEFT':
         new_head = (head[0] - BLOCK_SIZE, head[1])
     elif direction == 'RIGHT':
         new_head = (head[0] + BLOCK_SIZE, head[1])
+    #first new head is added
     snake.append(new_head)
     global food
+    #then if new_head did touch the food
     if snake[-1] == food:
         global score
-        score += 1
-
+        score += 1 #score gets up
+        #and new food randomly spawns
         food = (random.randint(0, WIDTH - BLOCK_SIZE) // BLOCK_SIZE * BLOCK_SIZE,
                 random.randint(0, HEIGHT - BLOCK_SIZE) // BLOCK_SIZE * BLOCK_SIZE)
     else:
-        snake.pop(0)
+        snake.pop(0) #if new head didn't touch the food then snakes tail gets cut off
 
 
 def check_collision():
@@ -59,8 +61,6 @@ def check_collision():
             head in snake[:-1]):
         return True
     return False
-
-
 
 
 if __name__ == '__main__':
